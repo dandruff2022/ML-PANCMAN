462898690e4c5d33d41c99dfa20b9763b0a7dba7
Peilin Li <li003997@umn.edu>
2025-02-08 19:21:16 -0600

    added realtime direction, change posture detection to always

--- src/components/DataCollection.js
+++ src/components/DataCollection.js
@@ -1,154 +1,178 @@
 import Webcam from "react-webcam";
 import { Grid, Button, Box } from "@mui/material";
 import {
-    ArrowUpward,
-    ArrowDownward,
-    ArrowBack,
-    ArrowForward,
+  ArrowUpward,
+  ArrowDownward,
+  ArrowBack,
+  ArrowForward,
 } from "@mui/icons-material/";
 import { useState, useRef } from "react";
 import { useAtom } from "jotai";
 import {
-    imgSrcArrAtom,
-    dataSetSizeAtom,
-    batchArrayAtom,
-    batchSizeAtom,
-    gameRunningAtom,
+  imgSrcArrAtom,
+  dataSetSizeAtom,
+  batchArrayAtom,
+  batchSizeAtom,
+  gameRunningAtom,
+  predictionAtom,
 } from "../GlobalState";
 
 const DIRECTIONS = {
-    up: <ArrowUpward />,
-    down: <ArrowDownward />,
-    left: <ArrowBack />,
-    right: <ArrowForward />,
+  up: <ArrowUpward />,
+  down: <ArrowDownward />,
+  left: <ArrowBack />,
+  right: <ArrowForward />,
 };
 
 export default function DataCollection({ webcamRef }) {
-    const [isCameraOn, setIsCameraOn] = useState(false);
+  const [isCameraOn, setIsCameraOn] = useState(false);
 
-    // ---- Model Training ----
-    const [imgSrcArr, setImgSrcArr] = useAtom(imgSrcArrAtom);
+  // ---- Model Training ----
+  const [imgSrcArr, setImgSrcArr] = useAtom(imgSrcArrAtom);
 
-    // ---- Configurations ----
-    const [, setBatchSize] = useAtom(batchSizeAtom);
-    const [gameRunning] = useAtom(gameRunningAtom);
+  // ---- Configurations ----
+  const [, setBatchSize] = useAtom(batchSizeAtom);
+  const [gameRunning] = useAtom(gameRunningAtom);
+  const [predictionDirection] = useAtom(predictionAtom);
 
-    // ---- UI Display ----
+  const directionMapping = {
+    0: "Right",
+    1: "Up",
+    2: "Left",
+    3: "Down",
+  };
 
-    const capture = (direction) => async () => {
-        // Capture image from webcam
-        const newImageSrc = webcamRef.current.getScreenshot();
+  const interpretedDirection =
+    directionMapping[predictionDirection] || "Unknown";
 
-        // If image is not null, proceed with adding it to the dataset
-        if (newImageSrc) {
+  // ---- UI Display ----
 
-            // Add example to the dataset
-            const newImageArr = [...imgSrcArr, { src: newImageSrc, label: direction }];
-            setImgSrcArr(newImageArr);
-            setBatchSize(Math.floor(newImageArr.length * 0.4));
-        }
-    };
+  const capture = (direction) => async () => {
+    // Capture image from webcam
+    const newImageSrc = webcamRef.current.getScreenshot();
 
-    const cameraPlaceholder = (
-        <Box
-            display="flex"
-            textAlign={"center"}
-            justifyContent="center"
-            alignItems="center"
-            sx={{
-                p: 2,
-                border: "1px dashed grey",
-                height: "224px",
-                width: "224px",
-                margin: "auto",
-                backgroundColor: "#ddd",
-            }}
-        >
-            Camera is off
+    // If image is not null, proceed with adding it to the dataset
+    if (newImageSrc) {
+      // Add example to the dataset
+      const newImageArr = [
+        ...imgSrcArr,
+        { src: newImageSrc, label: direction },
+      ];
+      setImgSrcArr(newImageArr);
+      setBatchSize(Math.floor(newImageArr.length * 0.4));
+    }
+  };
+
+  const cameraPlaceholder = (
+    <Box
+      display="flex"
+      textAlign={"center"}
+      justifyContent="center"
+      alignItems="center"
+      sx={{
+        p: 2,
+        border: "1px dashed grey",
+        height: "224px",
+        width: "224px",
+        margin: "auto",
+        backgroundColor: "#ddd",
+      }}
+    >
+      Camera is off
+    </Box>
+  );
+
+  return (
+    <Grid container>
+      {/* first row */}
+
+      <Grid
+        item
+        xs={12}
+        sx={{ marginBottom: 2 }}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+      >
+        <Box textAlign="center">
+          <Button
+            variant="contained"
+            onClick={() => setIsCameraOn(!isCameraOn)}
+            disabled={gameRunning}
+          >
+            {" "}
+            {isCameraOn ? "Stop" : "Start"} Camera
+          </Button>
         </Box>
-    );
+        <Box sx={{ marginTop: 1 }}>
+          {isCameraOn ? (
+            <Webcam
+              mirrored
+              width={224}
+              height={224}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              videoConstraints={{
+                width: 224,
+                height: 224,
+                facingMode: "user",
+              }}
+            />
+          ) : (
+            cameraPlaceholder
+          )}
+        </Box>
 
-    return (
-        <Grid container>
-            {/* first row */}
+        <Box
+          width={224}
+          height={112}
+          sx={{ marginTop: 1, textAlign: "center" }}
+        >
+          {isCameraOn
+            ? "Current Direction:" + interpretedDirection
+            : "Camera is off"}
+        </Box>
+      </Grid>
 
-            <Grid
-                item
-                xs={12}
-                sx={{ marginBottom: 2 }}
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                flexDirection="column"
-            >
-                <Box textAlign="center">
-                    <Button
-                        variant="contained"
-                        onClick={() => setIsCameraOn(!isCameraOn)}
-                        disabled={gameRunning}
-                    >
-                        {" "}
-                        {isCameraOn ? "Stop" : "Start"} Camera
-                    </Button>
-                </Box>
-                <Box sx={{ marginTop: 1 }}>
-                    {isCameraOn ? (
-                        <Webcam
-                            mirrored
-                            width={224}
-                            height={224}
-                            ref={webcamRef}
-                            screenshotFormat="image/jpeg"
-                            videoConstraints={{
-                                width: 224,
-                                height: 224,
-                                facingMode: "user",
-                            }}
-                        />
-                    ) : (
-                        cameraPlaceholder
-                    )}
-                </Box>
-            </Grid>
-
-            {Object.keys(DIRECTIONS).map((directionKey) => {
-                return (
-                    <OneDirection
-                        key={directionKey}
-                        disabled={!isCameraOn}
-                        directionIcon={DIRECTIONS[directionKey]}
-                        onCapture={capture(directionKey)}
-                        dirImgSrcArr={imgSrcArr.filter((d) => d.label == directionKey)}
-                    />
-                );
-            })}
-        </Grid>
-    );
+      {Object.keys(DIRECTIONS).map((directionKey) => {
+        return (
+          <OneDirection
+            key={directionKey}
+            disabled={!isCameraOn}
+            directionIcon={DIRECTIONS[directionKey]}
+            onCapture={capture(directionKey)}
+            dirImgSrcArr={imgSrcArr.filter((d) => d.label == directionKey)}
+          />
+        );
+      })}
+    </Grid>
+  );
 }
 
 const OneDirection = ({ directionIcon, onCapture, dirImgSrcArr, disabled }) => {
-    return (
-        <Grid item xs={3}>
-            <Box textAlign="center">
-                <Button
-                    variant="outlined"
-                    endIcon={directionIcon}
-                    onClick={onCapture}
-                    disabled={disabled}
-                >
-                    {" "}
-                    Add to{" "}
-                </Button>
-            </Box>
-            <Box textAlign="center" sx={{ width: "100%", height: "100px" }}>
-                {dirImgSrcArr.length > 0 && (
-                    <img
-                        height={"100%"}
-                        src={dirImgSrcArr[dirImgSrcArr.length - 1].src}
-                        style={{ padding: "2px" }}
-                    />
-                )}
-            </Box>
-        </Grid>
-    );
+  return (
+    <Grid item xs={3}>
+      <Box textAlign="center">
+        <Button
+          variant="outlined"
+          endIcon={directionIcon}
+          onClick={onCapture}
+          disabled={disabled}
+        >
+          {" "}
+          Add to{" "}
+        </Button>
+      </Box>
+      <Box textAlign="center" sx={{ width: "100%", height: "100px" }}>
+        {dirImgSrcArr.length > 0 && (
+          <img
+            height={"100%"}
+            src={dirImgSrcArr[dirImgSrcArr.length - 1].src}
+            style={{ padding: "2px" }}
+          />
+        )}
+      </Box>
+    </Grid>
+  );
 };
